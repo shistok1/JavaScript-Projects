294025a4faa17db1615a80ba7afccc16d1bb63f5
shistok1 <99694700+shistok1@users.noreply.github.com>
2022-03-21 14:42:47 -0700

    update main.js

--- Basic JavaScript Projects/JS/main.js
+++ Basic JavaScript Projects/JS/main.js
@@ -1,6 +1,6 @@
 var x = 16, y = 47, z = x+y;
 var string1 = "the first string", string2 = "the second string.";
 string3 = string1 + " + " + string2;
-string3.fontcolor("blue");
+string3 = string3.fontcolor("blue");
 window.alert(x + " + " + y + " = " + z);
 document.write(string3);
