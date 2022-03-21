3180cb4d0a4db2f2b7a4a2ab0100acc3057f9504
shistok1 <99694700+shistok1@users.noreply.github.com>
2022-03-21 09:42:05 -0700

    JS assignment 1

---
+++ Basic JavaScript Projects/JS/main.js
@@ -0,0 +1,6 @@
+var x = 16, y = 47, z = x+y;
+var string1 = "the first string", string2 = "the second string.";
+string3 = string1 + " + " + string2;
+string3.fontcolor("blue");
+window.alert(x + " + " + y + " = " + z);
+document.write(string3);
