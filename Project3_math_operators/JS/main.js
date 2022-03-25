94e2985ee4f708fd89e86dc1d7d5e08e6b857866
shistok1 <99694700+shistok1@users.noreply.github.com>
2022-03-25 14:00:50 -0700

    gfhfgdh

--- Project3_math_operators/JS/main.js
+++ Project3_math_operators/JS/main.js
@@ -1,5 +1,43 @@
-function subtractionFunction() {
-    var para = document.getElementById("Math");
-    var Subtraction = 5 - 2;
-    para.innerHTML = "5 - 2 = " + Subtraction;
+var simpleMath = null;
+var para = document.getElementById("Math");
+
+function subtraction() {
+    window.simpleMath = 5 - 2;
+    window.para = "5 - 2 = " + window.simpleMath;
+}
+
+function multiplication() {
+    window.simpleMath = 6 * 8;
+    para.window = "6 x 8 = " + window.simpleMath;
+}
+
+function division() {
+    window.simpleMath = 48 / 6;
+    para.innerHTML = "48 / 6 = " + window.simpleMath;
+}
+
+function longMath() {
+    window.simpleMath = 48 / 6 * 83 - 7;
+    para.innerHTML = "48 / 6 * 83 - 7 = " + window.simpleMath;
+}
+
+function modulus() {
+    window.simpleMath = 25 % 6;
+    para.innerHTML = "25 / 6 has a remainder of: " + window.simpleMath;
+}
+
+function negate() {
+    window.simpleMath = 10;
+    window.simpleMath = -window.simpleMath;
+    para.innerHTML = window.simpleMath;
+}
+
+function increment() {
+    window.simpleMath++;
+    para.innerHTML = window.simpleMath;
+}
+
+function random() {
+    window.simpleMath++;
+    para.innerHTML = window.simpleMath;
 }
